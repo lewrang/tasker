be3a2b95435a0665bc5043498638f99fcb85a256
Sabinka <cyzzzowna@gmail.com>
2015-05-05 13:00:03 +0000

    projects scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505124152) do
+ActiveRecord::Schema.define(version: 20150505125507) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",             default: ""
