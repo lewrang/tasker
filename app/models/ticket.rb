1abb2667ad39e49408af76bfa7e058af7a743a57
Sabinka <cyzzzowna@gmail.com>
2015-05-05 13:38:59 +0000

    twitter booststrap styles

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,3 @@
 class Ticket < ActiveRecord::Base
+  belongs_to :project
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505125507) do
+ActiveRecord::Schema.define(version: 20150505130427) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "priority"
+    t.string   "difficulty_level"
+    t.integer  "status"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "users", force: :cascade do |t|
